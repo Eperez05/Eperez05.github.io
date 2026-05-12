9da599338eacf9b9bca003d1dc4a7be08ec67a6f
Emily Perez <emilyp081905@gmail.com>
2026-05-12 10:21:47 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -33,9 +33,9 @@ const CONFIG = {
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Project Name',
+          title: 'Ecology Presentation on Knights Ferry',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            'This is an ecology presentation on Kights Ferry that talks about ecological concepts.',
           imageUrl:
             'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
           link: 'https://example.com',
